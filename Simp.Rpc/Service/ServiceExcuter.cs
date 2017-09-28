05f4c741d1b7a717351fbfa39adee266957aa7f5
刘大朋 <dapeng.liu@mtime.com>
2017-09-28 18:16:34 +0800

    提取server端执行器

---
+++ Simp.Rpc/Service/ServiceExcuter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Simp.Rpc.Service
+{
+    public class ServiceExcuter
+    {
+        public ServiceExcuter(object instance, RpcMethodInfo method)
+        {
+            Instance = instance;
+            Method = method;
+            ArgTypes = method?.RpcParameters.Select(param => param.Type).ToArray();
+            ReturnType = method?.RpcReturnType.Type;
+        }
+
+        public Type[] ArgTypes { get; private set; }
+
+        public Type ReturnType { get; private set; }
+
+        private object Instance { get; set; }
+
+        private RpcMethodInfo Method { get; set; }
+
+
+        public object Result { get; private set; }
+
+        public object Excute(object[] args)
+        {
+            Result = Method.MethodInfo.Invoke(Instance, args);
+            return Result;
+        }
+
+    }
+}
---
+++ Simp.Rpc/Service/SimpleRpcServiceContainer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Simp.Rpc.Service
+{
+    public class SimpleRpcServiceContainer : IRpcServiceContainer
+    {
+        private IDictionary<string, RpcServiceInfo> _rpcServiceTable;
+        private readonly IRpcServiceProvider _rpcServiceProvider;
+        private IServiceProvider serviceProvider;
+
+        public SimpleRpcServiceContainer(IRpcServiceProvider rpcServiceProvider)
+        {
+            this._rpcServiceProvider = rpcServiceProvider;
+            BuildRpcService();
+        }
+
+        public void BuildRpcService()
+        {
+            this._rpcServiceTable = this._rpcServiceProvider.ScanRpcServices();
+            var serviceCollection = new ServiceCollection();
+            foreach (var rpcServiceInfo in this._rpcServiceTable.Values)
+            {
+                if (rpcServiceInfo.IsImpl)
+                {
+                    serviceCollection.AddSingleton(rpcServiceInfo.ServiceType, rpcServiceInfo.ImplServiceType);
+                }
+            }
+            this.serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public ServiceExcuter LookupExecuter(string service, string method)
+        {
+            RpcServiceInfo rpcService;
+            if (!this._rpcServiceTable.TryGetValue(service, out rpcService))
+                throw new Exception($"service: {service} not found");
+
+            RpcMethodInfo rpcMethodInfo;
+            if (!rpcService.Methods.TryGetValue(method, out rpcMethodInfo))
+                throw new Exception($"method: {method} not found");
+
+            return new ServiceExcuter(serviceProvider.GetService(rpcService.ServiceType), rpcMethodInfo);
+        }
+
+        //public RpcServiceContainer BuildRpcServices(IDictionary<string, RpcServiceInfo> rpcServices)
+        //{
+        //    if (rpcServices == null || !rpcServices.Values.Any())
+        //        return this;
+
+        //    this._rpcServiceTable = rpcServices;
+        //    foreach (var rpcService in this._rpcServiceTable.Values)
+        //    {
+        //        if (rpcService.IsImpl)
+        //        {
+        //           //
+        //        }
+        //    }
+        //    return this;
+        //}
+
+        //public object Execute(string service, string method, RpcParameterInfo[] args)
+        //{
+        //    RpcServiceInfo rpcService;
+        //    if (!this._rpcServiceTable.TryGetValue(service, out rpcService))
+        //        throw new Exception($"service: {service} not found");
+
+        //    RpcMethodInfo rpcMethodInfo;
+        //    if (!rpcService.Methods.TryGetValue(method, out rpcMethodInfo))
+        //        throw new Exception($"method: {method} not found");
+
+        //    object result = rpcMethodInfo.MethodInfo.Invoke(rpcService.Instance, args.Select(arg => arg.Value).ToArray());
+
+        //    return result;
+        //}
+
+
+    }
+}
